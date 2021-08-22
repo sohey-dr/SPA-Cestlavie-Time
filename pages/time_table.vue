87a2fbb38397fdac7a0c687a45b235a82f3ef9f7
soheyHashimoto <heyso1209@gmail.com>
2021-08-22 22:36:34 +0900

    [ADD]バンドの配列作成

--- pages/time_table.vue
+++ pages/time_table.vue
@@ -66,6 +66,9 @@ export default {
   },
   methods: {
     outputTimeTable({ bandCount }) {
+      // バンドの配列作成 ex) bandCountが5なら ["バンド1", "バンド2", "バンド3", "バンド4", "バンド5"]
+      const bands = [...Array(Number(bandCount)).keys()].map(i => `バンド${++i}`);
+
       let time = this.$moment("2021-01-01T09:30:00")
       this.timeTable = `${time.format('HH:mm')}\nas`
       time.add(4, 'm')
