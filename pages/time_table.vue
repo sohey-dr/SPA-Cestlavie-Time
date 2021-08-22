882e06b13896c0fe4c63fd082d5bb65940b36ee8
soheyHashimoto <heyso1209@gmail.com>
2021-08-22 23:38:46 +0900

    [ADD]リハ時間を選択できるように

--- pages/time_table.vue
+++ pages/time_table.vue
@@ -5,7 +5,6 @@
         <div class="mb-4">
           <label
             class="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
           >
             バンド数を入力
           </label>
@@ -28,9 +27,34 @@
             placeholder="バンド数を入力"
           />
         </div>
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+          >
+            リハーサル時間
+          </label>
+          <input
+            v-model="rehearsalTime"
+            class="
+              shadow
+              appearance-none
+              border
+              rounded
+              w-full
+              py-2
+              px-3
+              text-gray-700
+              leading-tight
+              focus:outline-none
+              focus:shadow-outline
+            "
+            type="number"
+            placeholder="リハーサル時間を入力"
+          />
+        </div>
         <div class="w-28 mx-auto flex items-center justify-between">
           <button
-            @click="outputTimeTable({ bandCount })"
+            @click="outputTimeTable({ bandCount, rehearsalTime })"
             class="
               bg-blue-500
               hover:bg-blue-700
@@ -63,27 +87,29 @@ export default {
       bandCount: null,
       timeTable: "",
       time: null,
+      rehearsalTime: null,
     }
   },
   methods: {
-    outputTimeTable({ bandCount }) {
+    outputTimeTable({ bandCount, rehearsalTime }) {
+      this.timeTable = "";
       // バンドの配列作成 ex) bandCountが5なら ["バンド1", "バンド2", "バンド3", "バンド4", "バンド5"]
       const bands = [...Array(Number(bandCount)).keys()].map(i => `バンド${++i}`);
 
       this.time = this.$moment("2021-01-01T09:00:00");
-      this.rehearsal(bands);
+      this.rehearsal(bands, rehearsalTime);
       this.performance_preparation();
       this.performance(bands);
       return;
     },
-    rehearsal(bands) {
+    rehearsal(bands, rehearsalTime) {
       // 最初にだけ音出しバンドが存在する
-      this.timeTable += `${this.time.format('HH:mm')}〜${this.time.add(15, 'm').format('HH:mm')} 音出しバンド\n`;
+      this.timeTable += `${this.time.format('HH:mm')}〜${this.time.add(rehearsalTime, 'm').format('HH:mm')} 音出しバンド\n`;
       this.time.add(5, 'm'); 
 
       for (var i = bands.length - 1;i > -1;i--) {
         // タイムテーブルの肝の部分 ex) 15:00〜15:15 バンド1
-        this.timeTable += `${this.time.format('HH:mm')}〜${this.time.add(15, 'm').format('HH:mm')} ${bands[i]}\n`;
+        this.timeTable += `${this.time.format('HH:mm')}〜${this.time.add(rehearsalTime, 'm').format('HH:mm')} ${bands[i]}\n`;
         // 転換分で5分追加。顔合わせはすぐやるため最後のバンドの時は追加しない。
         if (i !== 0) {
          this.time.add(5, 'm'); 
