84b6fce28b2a0c9c744f4fbc6f9707f2f9569f42
soheyHashimoto <heyso1209@gmail.com>
2021-09-26 15:01:29 +0900

    Add AdminCard

---
+++ components/AdminCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="my-3">
+    <h2 class="text-lg font-semibold
+    ">幹部用機能ページ</h2>
+    <nuxt-link to="/time_table">
+      タイムテーブル自動作成はここから
+    </nuxt-link>
+  </div>
+</template>
